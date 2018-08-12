365c1e18ff878e26c2c850dc788a60e575e36adf
Gavin Soko <sokolofg@gmail.com>
2018-08-12 21:35:06 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,11 +1,12 @@
 #write your code here
 
 def countdown(num)
-  while num >= 1 
+  counter = num
+  while counter >= 1 
     puts "#{num} SECOND(S)!"
     
-  break if num == 0 
-  num -= 1
+  break if counter == 0 
+  counter -= 1
 end
     puts "HAPPY NEW YEAR!"
 end
