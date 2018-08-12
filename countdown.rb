e9518038d457d839f653e21dcb920d131c2c689c
Gavin Soko <sokolofg@gmail.com>
2018-08-12 21:45:57 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,9 +1,7 @@
-#write your code here
-
 def countdown(num)
-  while num > 0 
-    puts "#{num} SECOND(S)!"
-     num -= 1
-   end
-    puts "HAPPY NEW YEAR!"
+ while num > 0
+   puts "#{num} SECOND(S)!"
+  num -= 1
+ end
+  "HAPPY NEW YEAR!"
 end
